c7ef0de445f5201fd004a5314ef120bc0955517d
mgsyscyber-maker <mgsyscyber@gmail.com>
2026-08-10 17:22:13 +0330

    Refactor response handling and format in main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,9 @@ Deno.serve((req) => {
   const url = new URL(req.url);
 
   if (url.pathname === "/") {
-    return new Response("Hello from Mehran's Deno Lab! 🚀 VERSION 2");
+    return new Response(
+      "Hello from Mehran's Deno Lab! 🚀 VERSION 2",
+    );
   }
 
   if (url.pathname === "/status") {
@@ -18,14 +20,16 @@ Deno.serve((req) => {
       serverTime: new Date().toISOString(),
     });
   }
-if (url.pathname === "/info") {
-  return Response.json({
-    method: req.method,
-    url: req.url,
-    userAgent: req.headers.get("user-agent"),
-    timestamp: new Date().toISOString(),
-  });
-}
+
+  if (url.pathname === "/info") {
+    return Response.json({
+      method: req.method,
+      url: req.url,
+      userAgent: req.headers.get("user-agent"),
+      timestamp: new Date().toISOString(),
+    });
+  }
+
   return Response.json(
     {
       error: "Not Found",
